40226e4ac4ae34f7563d26842fe68cbe66b92b50
Leonardo Chuello <ljchuello@gmail.com>
2023-11-26 12:09:30 -0500

    .

--- Test/Program.cs
+++ Test/Program.cs
@@ -17,10 +17,6 @@ namespace Test
             {
                 LinodeClient linodeClient = new LinodeClient("apikey");
                 linodeClient = new LinodeClient(await File.ReadAllTextAsync("D:\\Linode.Api.txt"));
-
-                List<Region> list = await linodeClient.Region.Get();
-
-                Region region = await linodeClient.Region.Get(list[0].Id);
             }
             catch (Exception ex)
             {
